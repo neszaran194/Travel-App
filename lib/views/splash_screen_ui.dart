4be414359571d9937f057381a087bd9247082490
neszaran194 <neszaran.game@hotmail.com>
2024-07-07 01:37:00 +0700

    V1.1

--- lib/views/splash_screen_ui.dart
+++ lib/views/splash_screen_ui.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SplashScreenUi extends StatefulWidget {
   const SplashScreenUi({super.key});
@@ -13,13 +14,23 @@ class _SplashScreenUiState extends State<SplashScreenUi> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 214, 214, 214),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            CircularProgressIndicator(),
-            SizedBox(height: 20),
-            Text('Loading...'),
+            Image.asset('assets/images/logo.png', width: 150, height: 150),
+            SizedBox(height: MediaQuery.of(context).size.height*0.05,),
+            CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Color.fromARGB(255, 137, 211, 147)),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height*0.02,),
+            Text(
+              'Loading...',
+              style: GoogleFonts.itim(
+                fontSize: MediaQuery.of(context).size.height*0.03,
+              ),
+              ),
           ],
         ),
       ),
